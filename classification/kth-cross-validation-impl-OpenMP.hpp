f8a9f774d03505aec22923f8a58dd1ca20eb6473
johanna-codes <johacarvajalg@gmail.com>
2015-06-23 12:45:08 +1000

    try oPenMP

--- classification/kth-cross-validation-impl-OpenMP.hpp
+++ classification/kth-cross-validation-impl-OpenMP.hpp
@@ -153,7 +153,7 @@
 	   vec dist_segm_s = zeros<vec> (num_s);
 	   
 	   omp_set_num_threads(6);
-	   cout << omp_get_thread_num() << endl;
+	   cout << omp_get_num_threads() << endl;
 	   #pragma omp for
 	   for (int s_tr=0; s_tr<num_s; ++s_tr)
 	   {
