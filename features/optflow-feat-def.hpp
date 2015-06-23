8d69df750aeaa33decaf66c85f11b2a4f6292e71
johanna-codes <johacarvajalg@gmail.com>
2015-06-23 17:08:49 +1000

    Re-calculate for dim=14 features

--- features/optflow-feat-def.hpp
+++ features/optflow-feat-def.hpp
@@ -32,14 +32,16 @@ public:
   
   field<std::string> actions;
   
-  
-private:
-  struct feat_lab
+    struct feat_lab
   {
     vector <vec> features_video_i; //All vectors per video_i are stored here
     vector <int> labels_video_i;
     
   };
+  
+  
+private:
+
   
   
   inline feat_lab feature_video( std::string one_video );
