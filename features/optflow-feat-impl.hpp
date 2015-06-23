811e67e007078f24449dc7de7f0eb857ac155d6a
johanna-codes <johacarvajalg@gmail.com>
2015-06-23 15:59:57 +1000

    Re-calculate for dim=14 features

--- features/optflow-feat-impl.hpp
+++ features/optflow-feat-impl.hpp
@@ -34,7 +34,7 @@ opt_feat::features_all_videos( field<string> all_people )
   //{
     
     int sc = total_scene; //Solo estoy usando 1 
-    #pragma omp parallel for 
+    //#pragma omp parallel for 
     for (int pe = 0; pe< n_peo; ++pe)
     {
       for (int act=0; act<n_actions; ++act)
