979b3aad0b4e02ae50fe3c8ee4d5405a82be1b05
johanna-codes <johacarvajalg@gmail.com>
2015-07-08 19:50:46 +1000

    Run Grassmann PM

--- classification/kth-classification-svm.cpp
+++ classification/kth-classification-svm.cpp
@@ -19,8 +19,11 @@
 #include "kth-LogEucl-svm-cross-validation-def.hpp"
 #include "kth-LogEucl-svm-cross-validation-impl.hpp"
 
- #include "kth-SteinDiv-svm-cross-validation-def.hpp"
+#include "kth-SteinDiv-svm-cross-validation-def.hpp"
 #include "kth-SteinDiv-svm-cross-validation-impl.hpp"
+ 
+#include "kth-GrassPM-svm-cross-validation-def.hpp"
+#include "kth-GrassPM-svm-cross-validation-impl.hpp"
 
 
 
@@ -64,14 +67,19 @@ main(int argc, char** argv)
   all_people.load(peopleList);
   
   //Cross Validation LogEuclidean
-  cout << "Cross Validation for Log-Euclidean Distance" << endl;
-  kth_cv_svm_LogEucl run_kth_cv_svm_LogEucl(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
-  run_kth_cv_svm_LogEucl.run();
+//   cout << "Cross Validation for Log-Euclidean Distance" << endl;
+//   kth_cv_svm_LogEucl run_kth_cv_svm_LogEucl(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
+//   run_kth_cv_svm_LogEucl.run();
    
+//    //Cross Validation Stein Divergence
+//   cout << "Cross Validation for Stein Divergence" << endl;
+//   kth_cv_svm_Stein run_kth_cv_svm_SD(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
+//   run_kth_cv_svm_SD.run();
+  
    //Cross Validation Stein Divergence
-  cout << "Cross Validation for Stein Divergence" << endl;
-  kth_cv_svm_Stein run_kth_cv_svm_SD(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
-  run_kth_cv_svm_SD.run();
+  cout << "Cross Validation for Grassmann: PM" << endl;
+  kth_cv_svm_Grass_PM run_kth_cv_svm_PM(path, actionNames, all_people, scale_factor, shift, total_scenes,  dim);
+  run_kth_cv_svm_PM.run(p);
    
    
    //kth_CV_omp_onesegment.SteinDiv();
