a406b87e548baf3179717d548f98e5a19c955703
johanna-codes <johacarvajalg@gmail.com>
2015-07-16 15:40:15 +1000

    Riemannian and Grasmmann Kernels. Starting with Log_Eucl

---
+++ kernels/kth_test.m
@@ -0,0 +1,60 @@
+function kth_test(scale_factor, shift, sigma)
+
+
+RIEMANNIAN_KERNEL = @(X,Y,sigma) exp( -( dist_LogEuclidean(X,Y) )^2/(2*sigma^2) );
+
+path  = '~/codes/codes-git/study-paper/trunk/';
+dim = 14; 
+
+actions = importdata('actionNames.txt');
+all_people = importdata('people_list.txt');
+
+
+n_actions = size(actions,1);
+n_peo =  size(all_people,1);
+sc = 1;
+
+display('Testing svm + Kernel Log-Euclidean Distance');
+
+acc = 0;
+real_labels = zeros(n_peo*n_actions);
+est_labels  = zeros(n_peo*n_actions);
+  
+
+load_sub_path =strcat(path, 'cov_matrices/kth-one-cov-mat-dim', int2str(dim), '/sc', int2str(sc), '/scale', int2str(scale_factor), '-shift', int2str(shift) );
+
+j=1;
+  for pe_ts= 1: n_peo
+      
+      load_svm_model =strcat( './svm_models/logEucl_svm_run_', int2str(pe_ts), '_Sigma', int2str(sigma));
+      load load_svm_model;
+
+      for act_ts = 1:act_ts
+          real_labels(j) = act_ts;
+          name_load_cov = strcat( load_sub_path, '/LogMcov_', all_people(pe_ts), '_', actions(act_ts), '_dim', int2str(dim), '.h5');
+          hinfo = hdf5info( char(name_load_cov) );
+          one_video = hdf5read(hinfo.GroupHierarchy.Datasets(1));
+          X_test(:,:,1) = one_video;
+          
+          K_test = compute_kernel_svm(X_test,X_train, RIEMANNIAN_KERNEL,sigma);
+          [predict_label, accuracy, dec_values] = svmpredict([act_ts],[[1:size(K_test,1)]' K_test], model);
+          est_labels(j) = predict_label;
+          j=j+1;
+          
+          if response == act_ts
+              acc = acc+1;
+          end
+
+          
+          
+          
+          
+      end
+      
+     save_labels = strcat('./svm_results/LogEucl_scale', int2str(scale), '-shift', shift);     
+     save(save_labels, 'est_labels', 'real_labels');
+  
+  end
+  
+  acc*100/(n_peo*n_actions)
+   
--- kernels/kth_train.m
+++ kernels/kth_train.m
@@ -6,12 +6,12 @@ addpath('/home/johanna/toolbox/libsvm-3.20/matlab');% --> Wanda
 
 
 path  = '~/codes/codes-git/study-paper/trunk/';
-dim = 14; 
+dim = 14;
 
 actions = importdata('actionNames.txt');
 all_people = importdata('people_list.txt');
 scale_factor = 1;
-shift = 0; 
+shift = 0;
 
 n_actions = size(actions,1);
 n_peo =  size(all_people,1);
@@ -21,31 +21,33 @@ display('Training svm + Kernel Log-Euclidean Distance');
 
 n_test = (n_peo-1)*n_actions;
 
+%Entrenar aca y guardar para cada peo_test
+sigma = 1;
+RIEMANNIAN_KERNEL = @(X,Y,sigma) exp( -( dist_LogEuclidean(X,Y) )^2/(2*sigma^2) );
+
 load_sub_path =strcat(path, 'cov_matrices/kth-one-cov-mat-dim', int2str(dim), '/sc', int2str(sc), '/scale', int2str(scale_factor), '-shift', int2str(shift) );
-  for pe_ts= 1: n_peo
-      X_train = zeros(dim,dim,n_test);
-      labels_train = zeros(n_test,1);
-      k =1;
-      for pe_tr=1: n_peo
-          if pe_tr~=pe_ts
-              for act=1: n_actions
-                 name_load_cov = strcat( load_sub_path, '/LogMcov_', all_people(pe_tr), '_', actions(act), '_dim', int2str(dim), '.h5');
-                 hinfo = hdf5info( char(name_load_cov) );
-                 one_video = hdf5read(hinfo.GroupHierarchy.Datasets(1));
-                 X_train(:,:,k) = one_video;
-                 k=k+1;
-              end
-          end    
-      end
-      
-      %Entrenar aca y guardar para cada peo_test
-      sigma = 1;
-      RIEMANNIAN_KERNEL = @(X,Y,sigma) exp( -( dist_LogEuclidean(X,Y) )^2/(2*sigma^2) );
-      K_train = compute_kernel_svm(X_train,X_train, RIEMANNIAN_KERNEL, sigma);
-      model = svmtrain(labels_train, [[1:size(K_train,1)]' K_train], '-t 4 -q ');
-      save_svm_model =strcat( './svm_models/logEucl_svm_run_', int2str(pe_ts), '_Sigma', int2str(sigma));
-      save(save_svm_model, 'model');
-     
-  
-  end
-   
+for pe_ts= 1: n_peo
+    X_train = zeros(dim,dim,n_test);
+    labels_train = zeros(n_test,1);
+    k =1;
+    for pe_tr=1: n_peo
+        if pe_tr~=pe_ts
+            for act=1: n_actions
+                name_load_cov = strcat( load_sub_path, '/LogMcov_', all_people(pe_tr), '_', actions(act), '_dim', int2str(dim), '.h5');
+                hinfo = hdf5info( char(name_load_cov) );
+                one_video = hdf5read(hinfo.GroupHierarchy.Datasets(1));
+                X_train(:,:,k) = one_video;
+                k=k+1;
+            end
+        end
+    end
+    
+    
+    K_train = compute_kernel_svm(X_train,X_train, RIEMANNIAN_KERNEL, sigma);
+    model = svmtrain(labels_train, [[1:size(K_train,1)]' K_train], '-t 4 -q ');
+    save_svm_model =strcat( './svm_models/logEucl_svm_run_', int2str(pe_ts), '_Sigma', int2str(sigma));
+    save(save_svm_model, 'model', 'X_train');
+    
+    
+end
+
