663ed7f441ba5d23c593e44ecec3966eb92ca883
johanna-codes <johacarvajalg@gmail.com>
2015-07-19 12:32:13 +1000

    Testing

--- kernels/main_train_test.m
+++ kernels/main_train_test.m
@@ -7,18 +7,18 @@ addpath('/home/johanna/toolbox/libsvm-3.20/matlab');% --> Wanda
 path  = '~/codes/codes-git/study-paper/trunk/';
 sigma = 0.1:0.1:1;
 
-display('Training svm + Kernel Log-Euclidean Distance');
-ACC_train = zeros(length(sigma),25);
-parfor i=1:length(sigma)
-   sigma(i);
-   acc = kth_train_LogEuclidean(path, sigma(i));
-   ACC_train(i,:) = acc;
-    
-end
+% display('Training svm + Kernel Log-Euclidean Distance');
+% ACC_train = zeros(length(sigma),25);
+% parfor i=1:length(sigma)
+%    sigma(i);
+%    acc = kth_train_LogEuclidean(path, sigma(i));
+%    ACC_train(i,:) = acc;
+%     
+% end
 
  display('Testing svm + Kernel Log-Euclidean Distance');
  
- test_acc = zeros(length(sigma));
+ test_acc = zeros(length(sigma),1);
  scale = 1;
  shift = 0;
  
