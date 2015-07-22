bd8e439ec8d0e3ac8d5a7554da11ecb567715368
johanna-codes <johacarvajalg@gmail.com>
2015-07-22 11:16:07 +1000

    Run test. ProjPoly

--- kernels/kth_test_ProjectionPoly.m
+++ kernels/kth_test_ProjectionPoly.m
@@ -23,7 +23,7 @@ load_sub_path =strcat(path, 'grass_points/kth-grass-point-one-dim', int2str(dim)
 j=1;
   for pe_ts= 1: n_peo
       
-      load_svm_model =strcat( './svm_models/projPoly_svm_run_', int2str(pe_ts), '_degree', num2str(d),'.mat');
+      load_svm_model =strcat( './svm_models/projPoly_svm_run_', int2str(pe_ts), '_degree', num2str(p),'.mat');
       load(load_svm_model); %loading model and X_train
 
       for act_ts = 1:n_actions
