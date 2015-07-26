0ad63825090d3ac09bc9f9c3ae1489e2fa8df522
johanna-codes <johacarvajalg@gmail.com>
2015-07-26 21:20:34 +1000

    Updating

---
+++ dim_12_no_xy/classification/Dim12_onesegNN_kth_Grassmann_varying_p.m
@@ -0,0 +1,77 @@
+clear all
+clc
+close all
+%**********Using 12 features (Without x and %y)*****************
+
+
+%% One Grassmann Point per video. NN
+p=1:12;
+
+PM = [56.0000   53.0000   51.0000   54.0000   74.0000   93.0000   96.0000   88.0000   80.0000   91.0000   74.0000   19.0000];
+BC = [56.0000   53.0000   51.0000   54.0000   74.0000   93.0000   96.0000   88.0000   80.0000   91.0000   74.0000   31.0000];
+
+
+
+plot(p,PM, '-gd', 'LineWidth',3,'MarkerSize',5);
+hold on
+plot(p,BC, '-ro', 'LineWidth',3,'MarkerSize',5);
+legend('Projection Metric','Binet-Cauchy','Location', 'southwest');
+%title('Grassmann Manifolds','FontSize',30)
+ylabel('(%)','FontSize',20)
+xlabel('subspace order','FontSize',20)
+set(gca,'FontSize',20); 
+
+%Home
+print('/media/johanna/HD1T/latex-svn/study-paper-report/v3/images/Grass_varying_p_NN_dim12_no_xy', '-dpng','-r300');
+
+%% One Grassmann Point per video. SVM (using distances)
+ figure
+ PM_svm = [ 32.0000   40.0000   27.3333   32.0000   26.0000   41.3333   48.0000   52.6667   28.6667   32.0000   32.6667   16.6667];
+ BC_svm = [29.3333   44.0000   34.6667   27.3333   28.6667   48.0000   57.3333   54.6667   32.0000   38.6667   33.3333   16.6667];
+ 
+m_pm = max(PM_svm);
+m_bc = max(BC_svm);
+
+
+ plot(p,PM_svm, '-cd', 'LineWidth',3,'MarkerSize',5);
+ hold on
+ plot(p,BC_svm, '-mo', 'LineWidth',3,'MarkerSize',5);
+ legend('Projection Metric','Binet-Cauchy','Location', 'southwest');
+ ylabel('(%)','FontSize',20)
+ xlabel('subspace order','FontSize',20)
+ set(gca,'FontSize',20); 
+% %Home
+ print('/media/johanna/HD1T/latex-svn/study-paper-report/v3/images/Grass_varying_p_SVM_dim12_no_xy', '-dpng','-r300');
+% 
+
+%% Best performances
+close all
+all = [max(PM) max(BC); m_pm m_bc]; 
+
+Xlabel = {'One Segment + NN';'One Segment + SVM (dist)'};
+
+h =  bar(all);
+colormap('summer');
+ybuff=2;
+for i=1:length(h)
+    XDATA=get(get(h(i),'Children'),'XData');
+    YDATA=get(get(h(i),'Children'),'YData');
+    for j=1:size(XDATA,2)
+        x=XDATA(1,j)+(XDATA(3,j)-XDATA(1,j))/2;
+        y=YDATA(2,j)+ybuff;
+        t=[num2str(YDATA(2,j),3) ,'%'];
+        text(x,y,t,'Color','k','HorizontalAlignment','left','Rotation',90,'FontSize',15 )
+    end
+end
+
+ylim([0 109])
+set(gca,'XTickLabel',Xlabel,'FontSize',20);
+
+l{1}='ProjMet'; l{2}='BinCau';
+legend(h,l);
+
+
+
+%Home
+print('/media/johanna/HD1T/latex-svn/study-paper-report/v3/images/Grass_best_p_both_dim12_no_xy','-dpng','-r300');
+
--- dim_14/kernels/performance_kernels_II.m
+++ dim_14/kernels/performance_kernels_II.m
@@ -12,7 +12,7 @@ for i=1: length(all_p)
   all_p_mat(:,i) = all_p{i};
 end
 
-
+max_projRBF = max(max(all_p_mat));
 plot(delta,all_p_mat, '-d','LineWidth',3,'MarkerSize',3)
 %min_max = [min(delta), max(delta)+1];
 xlim([min(delta), max(delta)]);
@@ -27,7 +27,7 @@ legend('m=1', 'm=2', 'm=3' ,'m=4' ,'m=5', 'm=6', 'm=7', 'm=8', 'm=9', 'm=10', 'm
 %% ProjPoly Kernel
 figure
 load('projPoly_all_p_performance.mat');
-
+max_projPoly = max(test_acc);
 plot(p,test_acc, '-d','LineWidth',3,'MarkerSize',3)
 ylabel('(%)','FontSize',20)
 xlabel('# of subspaces','FontSize',20)
@@ -39,7 +39,7 @@ set(gca,'FontSize',20);
 %% LogEuclidean Kernel
 figure
 load('LogEucl_all_sigma_performance.mat');
-
+max_LogEucl = max(test_acc);
 plot(sigma,test_acc, '-gd','LineWidth',3,'MarkerSize',3)
 ylabel('(%)','FontSize',20)
 xlabel('\sigma','FontSize',20)
@@ -51,7 +51,7 @@ set(gca,'FontSize',20);
 %% LED-RBF Kernel
 figure
 load('LED_RBF_all_delta_performance.mat')
-
+max_ledRBF= max(test_acc);
 plot(delta,test_acc, '-md','LineWidth',3,'MarkerSize',3)
 ylabel('(%)','FontSize',20)
 xlabel('\delta','FontSize',20)
@@ -64,6 +64,7 @@ print('/media/johanna/HD1T/latex-svn/study-paper-report/v3/images/ledRBF_ker_del
 %% LED-POLY Kernel
 figure
 load('LED_POLY_all_n_performance.mat')
+max_ledPOLY= max(test_acc);
 plot(p,test_acc, '-bd','LineWidth',3,'MarkerSize',3)
 ylabel('(%)','FontSize',20)
 xlabel('D','FontSize',20)
@@ -71,3 +72,32 @@ set(gca,'FontSize',20);
 
 %Home
 print('/media/johanna/HD1T/latex-svn/study-paper-report/v3/images/ledPoly_ker_delta', '-dpng','-r300');
+
+%%ALL Best Perfomances
+close all
+all = [max_projRBF max_projPoly max_LogEucl max_ledRBF max_ledPOLY];
+
+
+Xlabel = {'projRBF';'projPoly'; 'LogEucl'; 'LED-RBF'; 'LED-POLY'};
+
+h =  bar(all);
+colormap('summer');
+ybuff=2;
+for i=1:length(h)
+    XDATA=get(get(h(i),'Children'),'XData');
+    YDATA=get(get(h(i),'Children'),'YData');
+    for j=1:size(XDATA,2)
+        x=XDATA(1,j)+(XDATA(3,j)-XDATA(1,j))/2;
+        y=YDATA(2,j)+ybuff;
+        t=[num2str(YDATA(2,j),3) ,'%'];
+        text(x,y,t,'Color','k','HorizontalAlignment','left','Rotation',90,'FontSize',15 )
+    end
+end
+
+ylim([0 109])
+set(gca,'XTickLabel',Xlabel,'FontSize',15);
+
+print('/media/johanna/HD1T/latex-svn/study-paper-report/v3/images/summary_ker','-dpng','-r300');
+
+% l{1}='ProjMet'; l{2}='BinCau';
+% legend(h,l);
